110be12a3d87cc40c1cf14c1f516d303c22c11ca
MiguelARam27 <garandster27@live.com>
2021-01-15 02:50:18 -0500

    Added static page to edit

--- index.php
+++ index.php
@@ -2,21 +2,7 @@
 <div class="projects--container" id="#home">
     <h1 class="projects--container__title u-margin-top-small u-margin-bottom-small">portfolio</h1>
     <div class="projects--container__projects-grid ">
-        <div class="project-card">
-            <div alt="" class="project-card__img" src="">
-                <img alt="" src="">
-                <div class="project-card__img__title">
-                    <h1>Project 1</h1>
-                </div>
-            </div>
-            <div class="project-card__info">
-                <div class="project-card__info__color1"></div>
-                <div class="project-card__info__color2"></div>
-                <a class="project-card__info__button" href="#popup">Learn more</a>
-                <a class="project-card__info__button" href="">Code</a>
-                <a class="project-card__info__button" href="">Demo</a>
-            </div>
-        </div>
+
         <?php
                     $mypod = pods('project');
                     $mypod->find('name ASC');
@@ -74,9 +60,6 @@
         </div>
         <div class="popup__content__gallery-container">
             <div class="gallery js-flickity">
-                <div class="gallery-cell">
-                    <img alt="" src="https://images.unsplash.com/photo-1593284094481-503ee97959be?ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80">
-                </div>
                 <?php echo $content?>
                  <?php
                     $max_slides = 8; // this should be the maximum number of skills
@@ -132,53 +115,7 @@
     </div>
 </div>
 <?php endwhile;?>
-<div class="popup" id="popup">
-    <div class="popup__content">
-        <a class="popup__close" href="#home">&times;</a>
-        <div class="popup__content__title ">
-            <h1>Project1 lol</h1>
-        </div>
-        <div class="popup__content__gallery-container">
-            <div class="gallery js-flickity">
-                <div class="gallery-cell">
-                    <img alt="" src="https://images.unsplash.com/photo-1593284094481-503ee97959be?ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80">
-                </div>
-                <div class="gallery-cell">
-                    <img alt="" src="assets/example1.png">
-                </div>
-                <div class="gallery-cell"></div>
-                <div class="gallery-cell"></div>
-                <div class="gallery-cell"></div>
-            </div>
-        </div>
-        <div class="popup__content__info">
-            <div class="popup__content__info__column">
-                <h2>summary</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae veniam, fugiat, tempore consequuntur rerum corporis recusandae non debitis quasi molestias ipsa ut nostrum repellat ex hic odit cum? Laboriosam, neque.</p>
-            </div>
-            <div class="popup__content__info__column">
-                <h2>background</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae veritatis quas fugiat repellat minus magnam qui blanditiis laborum perspiciatis. Hic dolorum saepe quaerat doloribus similique officiis recusandae sequi officia perspiciatis.</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae veritatis quas fugiat repellat minus magnam qui blanditiis laborum perspiciatis. Hic dolorum saepe quaerat doloribus similique officiis recusandae sequi officia perspiciatis.</p>
-            </div>
-        </div>
-        <div class="popup__content__skills">
-            <h1>Skills used</h1>
-            <div class="popup__content__skills__skills-container">
-                <span class="popup__content__skills__skills-container__skill">Html</span>
-                <span class="popup__content__skills__skills-container__skill">Java</span>
-                <span class="popup__content__skills__skills-container__skill">Python</span>
-                <span class="popup__content__skills__skills-container__skill">CSS</span>
-                <span class="popup__content__skills__skills-container__skill">Math</span>
-                <span class="popup__content__skills__skills-container__skill">noobs</span>
-            </div>
-        </div>
-        <div class="popup__content__links">
-            <a class="popup__content__links__button" href="">Code</a>
-            <a class="popup__content__links__button" href=" ">Demo</a>
-        </div>
-    </div>
-</div>
+
 
 
 <?php get_footer(); ?> 
